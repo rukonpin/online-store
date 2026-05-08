204910b5ab550393eecbf3ff8bbcc877672c2ffb
Ruslan Konkov <pinkiandbrain45@gmail.com>
2026-05-08 14:32:39 +0300

    feat: Add static resources, styles, and initial product catalog page

---
+++ src/main/java/com/online/store/controller/product/web/ProductViewController.java
@@ -0,0 +1,40 @@
+package com.online.store.controller.product.web;
+
+import com.online.store.dto.product.ProductDto;
+import com.online.store.mapper.product.ProductMapper;
+import com.online.store.service.product.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/products")
+@RequiredArgsConstructor
+public class ProductViewController {
+
+    private final ProductService productService;
+    private final ProductMapper productMapper;
+
+    @GetMapping
+    public String catalog(
+            @RequestParam(required = false) String query,
+            @PageableDefault Pageable pageable,
+            Model model) {
+
+        Page<ProductDto> products = productService.getAll(query, pageable)
+                .map(productMapper::toDto);
+
+        model.addAttribute("products", products);
+        model.addAttribute("query", query);
+
+        return "index";
+    }
+}
--- src/test/java/com/online/store/StoreApplicationTests.java
+++ src/test/java/com/online/store/StoreApplicationTests.java
@@ -2,9 +2,18 @@ package com.online.store;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class StoreApplicationTests {
+
+	@Container
+	@ServiceConnection
+	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine");
 
 	@Test
 	void contextLoads() {
