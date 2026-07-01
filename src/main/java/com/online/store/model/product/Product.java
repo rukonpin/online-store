58a1a625163b6a2d7b9ec1df01b7c248c9296bbc
Ruslan Konkov <pinkiandbrain45@gmail.com>
2026-07-01 21:35:46 +0300

    refactor(order): migrate Order model and repository to R2DBC

--- src/main/java/com/online/store/model/product/Product.java
+++ src/main/java/com/online/store/model/product/Product.java
@@ -1,43 +1,58 @@
 package com.online.store.model.product;
 
-import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+import org.springframework.lang.Nullable;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-@Entity
 @Getter
 @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
-@Table(name = "products")
-public class Product {
+@Table("products")
+public class Product implements Persistable<UUID> {
 
     @Id
-    @GeneratedValue
-    @Column(name = "product_uuid", updatable = false, nullable = false)
-    private UUID uuid;
+    @Column("product_uuid")
+    private UUID productUuid;
 
-    @Column(nullable = false)
+    @Column("name")
     private String name;
 
-    @Column(columnDefinition = "TEXT")
+    @Column("description")
     private String description;
 
-    @Column(nullable = false)
+    @Column("image_url")
     private String imageUrl;
 
-    @Column(precision = 10, scale = 2, nullable = false)
+    @Column("price")
     private BigDecimal price;
 
-    @CreationTimestamp
+    @CreatedDate
+    @Column("created_at")
     private LocalDateTime createdAt;
 
-    @UpdateTimestamp
+    @LastModifiedDate
+    @Column("updated_at")
     private LocalDateTime updatedAt;
+
+    @Nullable
+    @Override
+    public UUID getId() {
+        return this.productUuid;
+    }
+
+    @Override
+    public boolean isNew() {
+        return this.createdAt == null;
+    }
 }
--- src/main/java/com/online/store/repository/product/ProductRepository.java
+++ src/main/java/com/online/store/repository/product/ProductRepository.java
@@ -1,13 +1,21 @@
 package com.online.store.repository.product;
 
 import com.online.store.model.product.Product;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.r2dbc.repository.Query;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
-public interface ProductRepository extends JpaRepository<Product, UUID> {
+public interface ProductRepository extends R2dbcRepository<Product, UUID> {
+    Flux<Product> findByNameContainingIgnoreCase(String query, Pageable pageable);
+    Mono<Long> countByNameContainingIgnoreCase(String query);
 
-    Page<Product> findByNameContainingIgnoreCase(String query, Pageable pageable);
+    @Query("SELECT * FROM products ORDER BY price ASC LIMIT :limit OFFSET :offset")
+    Flux<Product> findAllOrderByPriceAsc(int limit, long offset);
+
+    @Query("SELECT * FROM products ORDER BY price DESC LIMIT :limit OFFSET :offset")
+    Flux<Product> findAllOrderByPriceDesc(int limit, long offset);
 }
